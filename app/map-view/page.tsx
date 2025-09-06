093b33c3cd382de518e85fd86b6bb7f5af61d842
Thomas Lombardi <thomas.lombardi@gmail.com>
2025-09-06 18:00:58 -0500

    add suspense to pages

--- app/map-view/page.tsx
+++ app/map-view/page.tsx
@@ -1,5 +1,10 @@
+import { Suspense } from 'react';
 import MapView from './MapView';
 
 export default async function Page() {
-  return <MapView />;
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <MapView />
+    </Suspense>
+  );
 }
